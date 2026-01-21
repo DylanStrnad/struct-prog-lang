d59bf96d443096f5ed2e2cfec2de444fc44f2c44
DylanStrnad <dylanstrnad@outlook.com>
2026-01-21 22:02:28 +0000

    tokenizer

--- personal/tokenizer.py
+++ personal/tokenizer.py
@@ -1,9 +1,42 @@
 import re
 
 
-p = re.compile("ab*")
+# p = re.compile("ab*")
 
-if p.match("a") :
-    print("match")
-else :
-    print("not match")
+# if p.match("a") :
+#     print("match")
+# else :
+#     print("not match")
+
+#ON MIDTERM EXAM
+#cannot solve with regex:
+    #bbabb
+    #bbbabbb
+    #bbbbabbb
+
+patterns = [
+    (r"\s+", "whitespace"),
+    (r"\d+", "number"),
+    (r"\+", "+"),
+    (r"\-", "-"),
+    (r"\/", "/"),
+    (r"\*", "*"),
+    (r"\.", "error")
+]
+
+patterns = [(re.compile(p), tag) for p,tag in patterns]
+
+def tokenize(characters): 
+    "tokenize a string using the patterns above"
+    tokens = []
+    position = 0
+    line = 1
+    column = 1
+    while position < len(characters):
+        for pattern, tag in patterns:
+            match = pattern.match(characters, position)
+            if match:
+                break
+        assert match is not None
+
+        
