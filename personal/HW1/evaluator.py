9c03e367cc99e4c8763518e54f6a911037a21775
DylanStrnad <dylanstrnad@outlook.com>
2026-02-11 18:57:08 +0000

    Adding Modulus Operator

---
+++ personal/HW1/evaluator.py
@@ -0,0 +1,66 @@
+import parser
+
+def evaluate(ast):
+    if ast["tag"] == "number":
+        return ast["value"]
+    elif ast["tag"] == "+":
+        return evaluate(ast["left"]) + evaluate(ast["right"])
+    elif ast["tag"] == "-":
+        return evaluate(ast["left"]) - evaluate(ast["right"])
+    elif ast["tag"] == "*":
+        return evaluate(ast["left"]) * evaluate(ast["right"])
+    elif ast["tag"] == "/":
+        return evaluate(ast["left"]) / evaluate(ast["right"])
+    elif ast["tag"] == "%":
+        return evaluate(ast["left"]) % evaluate(ast["right"])
+    else:
+        raise ValueError(f"Unknown AST node: {ast}")
+
+def test_evaluate():
+    print("test evaluate()")
+    ast = {"tag": "number", "value": 3}
+    assert evaluate(ast) == 3
+    ast = {
+        "tag": "+",
+        "left": {"tag": "number", "value": 3},
+        "right": {"tag": "number", "value": 4},
+    }
+    assert evaluate(ast) == 7
+    ast = {
+        "tag": "*",
+        "left": {
+            "tag": "+",
+            "left": {"tag": "number", "value": 3},
+            "right": {"tag": "number", "value": 4},
+        },
+        "right": {"tag": "number", "value": 5},
+    }
+    assert evaluate(ast) == 35
+    ast = {
+        "tag": "%",
+        "left": {"tag": "number", "value": -5},
+        "right": {"tag": "number", "value": 10},
+    }
+    assert evaluate(ast) == 5
+
+    ast = {
+        "tag": "%",
+        "left": {
+            "tag": "-",
+            "left": {"tag": "number", "value": 3},
+            "right": {"tag": "number", "value": 7},
+        },
+        "right": {"tag": "number", "value": 10},
+    }
+    assert evaluate(ast) == 6
+
+    ast = {
+        "tag": "%",
+        "left": {"tag": "number", "value": 2},
+        "right": {"tag": "number", "value": -4},
+    }
+    assert evaluate(ast) == -2
+
+if __name__ == "__main__":
+    test_evaluate()
+    print("done.")
